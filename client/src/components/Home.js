2f8011e514e7824d27882e7f276854694df4356c
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-02-28 14:01:06 +0530

    Create Home.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+return (
+    <div className='text-center pt-5'>
+        <h1 className='text-secondary'>WELCOME TO THE</h1>
+        <h1>PROJECT SHOP-ENDS</h1>
+    </div>
+)
+}
+
+export default Home
